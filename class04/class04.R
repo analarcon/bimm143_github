97734c985464a74457997446d709b36fd538c0c6
Anyoleth Alarcon <analarcon@ucsd.edu>
2025-05-20 13:20:10 -0700

    Adding

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Anyoleth Alarcon, A17347293"
+#' PID: "A17347293"
+#' ---
+
+#My first R script
+x<-1:50
+plot(x,sin(x))
+plot(x,sin(x),typ="l",col="green",lwd=3, 
+     xlab="Silly x axis",ylab="Sensible y axis")
+     
